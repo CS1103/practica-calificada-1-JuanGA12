b445247ddbdada3d41c6877aabcd265763a856bc
JuanGA12 <juan.galvez@utec.edu.pe>
2019-09-12 17:58:56 -0500

    Examen
    .

--- Polynomial.cpp
+++ Polynomial.cpp
@@ -5,3 +5,59 @@
 #include <stdexcept>
 #include <iostream>
 #include "Polynomial.h"
+
+Polynomial::Polynomial():polinomio{nullptr}, exponente{0}, coeficiente{0} {
+//todos los valores a 0;
+}
+
+Polynomial::Polynomial(unsigned int j):exponente{j} {
+    polinomio = new Polynomial[exponente];//van haber tantos polimonios como el numero de exponente + 1;
+    //ejemplo exponente 2, 1 + 2x^1 + 4x^2, # de polinomios = 3 = 2 + 1;
+
+}
+
+void Polynomial::add(unsigned int expo, float coe) {
+
+    auto tmp = new Polynomial[exponente + 1];
+
+    for (int i = 0; i < exponente; ++i) {
+        tmp[i] = polinomio[i];
+    }
+
+    delete [] polinomio;
+
+    polinomio = tmp;
+
+    //polinomio[exponente] = se deberia igualar a "coeficientex^exponente"
+
+    exponente ++;
+
+}
+
+void Polynomial::grado(Polynomial &) {
+    cout<<exponente;
+}
+
+Polynomial::~Polynomial() {
+    delete [] polinomio;
+}
+
+string Polynomial::get_expression(Polynomial &p) {
+    return std::string();
+}
+
+Polynomial Polynomial::operator+(const Polynomial & p1) {
+    Polynomial tmp(p1);
+    for (int j = 0; j < p1.exponente; j++) {
+        tmp.polinomio[j] = tmp.polinomio[j] + this->polinomio[j];
+    }
+
+    return tmp;
+}
+
+Polynomial Polynomial::operator+=(const Polynomial &) {
+
+}
+
+
+
--- Polynomial.h
+++ Polynomial.h
@@ -4,12 +4,26 @@
 
 #ifndef POLINOMIO_POLINOMIO_H
 #define POLINOMIO_POLINOMIO_H
-
+#include <string>
 #include <vector>
+
 using namespace std;
 
 class Polynomial {
+private:
+    unsigned int exponente;
+    float coeficiente;
+    Polynomial *polinomio;
+public:
 
+    Polynomial();
+    ~Polynomial();
+    Polynomial(unsigned int);
+    void add(unsigned int, float);
+    void grado(Polynomial&);
+    string get_expression(Polynomial& p);
+    Polynomial operator + (const Polynomial&);
+    Polynomial operator +=(const Polynomial&);
 };
 
 #endif //POLINOMIO_POLINOMIO_H
